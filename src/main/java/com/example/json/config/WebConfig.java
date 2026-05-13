fb05c169dcfaa2e881d4e5186d0ab7989ab020e2
tgldrchn <tuguldur9190@gmail.com>
2026-05-13 10:58:58 +0800

    fix interceptor path patterns

--- src/main/java/com/example/json/config/WebConfig.java
+++ src/main/java/com/example/json/config/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/users/**")
-                .excludePathPatterns("/users");
+                .addPathPatterns("/users");
     }
 }
