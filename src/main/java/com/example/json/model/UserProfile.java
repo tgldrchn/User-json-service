fad74673303ca5c4887179ca0d4e28aec400d497
tgldrchn <tuguldur9190@gmail.com>
2026-05-13 10:36:17 +0800

    fix userId and id separation

--- src/main/java/com/example/json/model/UserProfile.java
+++ src/main/java/com/example/json/model/UserProfile.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class UserProfile {
     @Id
+    private String id; 
     private String userId;
     private String name;
     private String email;
